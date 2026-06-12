20536e068379d533973695dc04844fed8083da18
Parmis~ <parmiskouoshki@gmail.com>
2026-06-13 02:29:28 +0330

    Fix: Wire Ask Endpoint To celery tasks

--- app/api/routes/ask.py
+++ app/api/routes/ask.py
@@ -1,29 +1,34 @@
+from celery.result import AsyncResult
 from fastapi import APIRouter , status , HTTPException
 from fastapi.responses import JSONResponse
 from models.models import AskRequest , AskResponse
+from celery_tasks.Celery_tasks import *
 
 
 router = APIRouter(prefix="/api/v1" , tags=["Ask"])
 
 @router.post("/ask")
-async def ask_question(request : AskRequest):
-    if request.question.strip() == "":
-        raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST , detail = "Question cannot be empty")
-    return JSONResponse(status_code = status.HTTP_202_ACCEPTED , content = {'task_id' : "Task_123" , 'status' : "processing"}) 
+def ask(payload: AskRequest):
+    task = ask_rag.delay(
+        question=payload.question,
+        conversation_id=payload.conversation_id,
+    )
+
+    return {
+        "task_id": task.id
+    }
 
 
+@router.get("/answer/{task_id}" , response_model=None)
+async def answer(task_id: str):
+    result = AsyncResult(task_id, app=celery_app)
 
-@router.get("/answer/{task_id}" , response_model=AskResponse)
-async def get_answer(task_id : str):
-    if task_id != 'Task_123':
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND , detail='Task not found')
-    response = AskResponse(answer = 'FastAPI is a modern Python web framework.',
-                           conversation_id = 'cpnv_123' , 
-                           retrieved_documnets = [{
-                               "document_id" : 'doc_1' , 
-                               "chunk_id" : 'chunk_1' ,
-                               "document_source_id" : 'source_1' , 
-                               "score" : 0.95
-                           }])
-    
-    return response
+    if not result.ready():
+        return {
+            "status": "processing"
+        }
+
+    return {
+        "status": "done",
+        "data": result.get()
+    }
