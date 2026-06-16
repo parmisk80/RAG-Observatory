d173b060dfa02b4e72906d3dc9b0d444e2cedc16
Parmis~ <parmiskouoshki@gmail.com>
2026-06-16 22:32:18 +0330

    fix : remove hardcoded response model and connect to celery task

--- app/api/routes/evaluation.py
+++ app/api/routes/evaluation.py
@@ -1,15 +1,25 @@
 from fastapi import APIRouter , status , HTTPException
-from fastapi.responses import JSONResponse
 from models.models import  EvaluationRequest , EvaluationResponse 
+from celery_tasks.Celery_tasks import evaluation_report , celery_app
+from celery.result import AsyncResult
 
 
 router = APIRouter(prefix='/api/v1/evaluation' , tags=['Evaluation'])
 
-@router.post("/evaluate" , response_model= EvaluationResponse)
-async def evaluate_generated_answers(evaluate_process : EvaluationRequest):
-    if evaluate_process.generated_answer.strip == "":
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST , detail="Generated answers cannot be empty")
-    
-    response = EvaluationResponse(faithfulness_score = 0.91 ,relevance_score = 0.88 , correctness_score = 0.93)
+@router.get("/result/{task_id}")
+async def retrieval_result(task_id: str):
 
-    return JSONResponse(status_code=status.HTTP_200_OK ,content=response.model_dump())
+    result = AsyncResult(task_id, app=celery_app)
+
+    if result.state in ("PENDING", "STARTED"):
+        return {"status": "processing"}
+
+    if result.state == "FAILURE":
+        return {"status": "failed"}
+
+    if result.state == "SUCCESS":
+        return {
+            "status": "completed",
+            "result": result.get()}
+
+    return {"status": "processing"}
